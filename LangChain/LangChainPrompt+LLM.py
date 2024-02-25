01d6e8c049b06e590786b2914d05d3ac4b1177ff
Ravi Godara <rgodara@cisco.com>
2024-02-25 10:55:43 +0530

    LangChainSimpleExample

--- LangChain/LangChainPrompt+LLM.py
+++ LangChain/LangChainPrompt+LLM.py
@@ -1,7 +1,20 @@
-from LangChainAzureBase import llm
 from langchain_core.prompts import ChatPromptTemplate
+from LangChainAzureBase import llm
 
-prompt = ChatPromptTemplate.from_template("Tell me a joke about following What is common betweent {topic1} and {topic2}")
-chain = prompt | llm
-res =   llm.invoke(input("Enter the query to search: "))
-print(res)
+def search_joke(topic1, topic2):
+    """
+    This function searches for a joke related to two given topics using LangChain chaining prompt and LLM.
+
+    Args:
+        topic1 (str): The first topic.
+        topic2 (str): The second topic.
+
+    Returns:
+        str: The joke related to the given topics.
+    """
+    prompt = ChatPromptTemplate.from_template("Tell me a joke about following What is common between {topic1} and {topic2}")
+    chain = prompt | llm
+    res = llm.invoke(input("Enter the query to search: "))
+    return res
+
+print(search_joke("cats", "dogs"))
